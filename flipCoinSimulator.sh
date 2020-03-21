4f3fa4778ce3e80a4d55dfa7e2307adcbbed63e7
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-21 21:47:11 +0530

    Added:calculated doublet combination along with percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,23 +1,45 @@
 #!/bin/bash -x
+#VARIABLE
+HH=0
+HT=0
+TT=0
+TH=0
+flip=0
+
 echo "Flip Coin Simulation"
 declare -A dictionary
-for (( i=0;i<10;i++ ))
+
+#DOUBLET combination
+while [ $flip -lt 10 ]
 do
-	coin=$((RANDOM%2))
-	if [[ $coin -eq 1 ]]
+	coin1=$((RANDOM%2))
+	coin2=$((RANDOM%2))
+	if [[ $coin1 -eq 1 && $coin2 -eq 1 ]]
 	then
-		((head++))
+		((HH++))
+
+	elif [[ $coin1 -eq 1 && $coin2 -eq 0 ]]
+	then
+		((HT++))
+
+	elif [[ $coin1 -eq 0 && $coin -eq 1 ]]
+	then
+		((TH++))
+
 	else
-		((tail++))
+		((TT++))
 	fi
+	((flip++))
 done
 
-dictionary[0]=$head
-dictionary[1]=$tail
-echo "${!dictionary[@]}":"${dictionary[@]}"
+#percentage of doublet
 
-result1=$((($head*100)/10))
-result2=$((($tail*100)/10))
+result1=`echo "scale=2; $HH*100/10" | bc `
+result2=`echo "scale=2; $HT*100/10" | bc `
+result3=`echo "scale=2; $TH*100/10" | bc `
+result4=`echo "scale=2; $TT*100/10" | bc `
+
+
 
 
 
