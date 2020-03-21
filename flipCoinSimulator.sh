d3927b29a187e620c79c72cd402a2a17e4226ad1
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-21 16:16:49 +0530

    Added:flipping the coin and store the combination of head and tail in dictionary,and print the percentage of head and tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,23 @@
 #!/bin/bash -x
 echo "Flip Coin Simulation"
-coin=$((RANDOM%2))
-if [[ $coin -eq 1 ]]
-then
-		echo "Head"
-else
-		echo "Tail"
-fi
+declare -A dictionary
+for (( i=0;i<10;i++ ))
+do
+	coin=$((RANDOM%2))
+	if [[ $coin -eq 1 ]]
+	then
+		((head++))
+	else
+		((tail++))
+	fi
+done
+
+dictionary[0]=$head
+dictionary[1]=$tail
+echo "${!dictionary[@]}":"${dictionary[@]}"
+
+result1=$((($head*100)/10))
+result2=$((($tail*100)/10))
+
+
+
