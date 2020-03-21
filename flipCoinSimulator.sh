99fa60ba738e9dd10aebf12d2e7b8a3a4a4d2015
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-22 01:39:15 +0530

    Added:sorted triplet combination and print maximum time win combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -15,7 +15,6 @@ THH=0
 HTH=0
 flip=0
 
-read -p "enter number of time coin toss:" number
 #triplet combination
 while [ $flip -lt 10 ]
 do
@@ -25,35 +24,28 @@ do
 	if [[ $coin1 -eq 1 && $coin2 -eq 1 && $coin3 -eq 1 ]]
 	then
 		((HHH++))
-
 	elif [[ $coin1 -eq 1 && $coin2 -eq 1 && $coin3 -eq 0 ]]
 	then
 		((HHT++))
-
 	elif [[ $coin1 -eq 1 && $coin2 -eq 0 && $coin3 -eq 1 ]]
 	then
 		((HTH++))
-
 	elif [[ $coin1 -eq 0 && $coin2 -eq 1 && $coin3 -eq 1 ]]
 	then
 		((THH++))
-
-   elif [[ $coin1 -eq 0 && $coin2 -eq 1 && $coin3 -eq 0 ]]
+ 	elif [[ $coin1 -eq 0 && $coin2 -eq 1 && $coin3 -eq 0 ]]
 	then
 		((THT++))
-
-   elif [[ $coin1 -eq 0 && $coin2 -eq 0 && $coin3 -eq 0 ]]
-   then
+	elif [[ $coin1 -eq 0 && $coin2 -eq 0 && $coin3 -eq 0 ]]
+	then
 		((TTT++))
-
-   elif [[ $coin1 -eq 0 && $coin2 -eq 0 && $coin3 -eq 1 ]]
-   then
-   	((TTH++))
-
-   else
+ 	elif [[ $coin1 -eq 0 && $coin2 -eq 0 && $coin3 -eq 1 ]]
+	then
+		((TTH++))
+	else
 		((HTT++))
-   fi
-	((flip++))
+	fi
+		((flip++))
 done
 
 #add value in dictionary
@@ -65,6 +57,8 @@ dictionary[4]=$THT
 dictionary[5]=$TTT
 dictionary[6]=$TTH
 dictionary[7]=$HTT
+
+#print dictionary
 echo "${!dictionary[@]}"
 echo "${dictionary[@]}"
 
@@ -78,6 +72,21 @@ result6=`echo "scale=2; $TTT*100/10" | bc `
 result7=`echo "scale=2; $TTH*100/10" | bc `
 result8=`echo "scale=2; $HTT*100/10" | bc `
 
+#sorted triplet combination
+for (( i=0;i<8;i++ ))
+do
+	for (( j=$((i+1));j<8;j++ ))
+	do
+		if [[ ${dictionary[$i]} -gt ${dictionary[$j]} ]]
+		then
+			temp=${dictionary[$i]}
+			dictionary[$i]=${dictionary[$j]}
+			dictionary[$j]=$temp
+		fi
+	done
+done
+echo "${dictionary[@]}"
+echo "maximum time win combination:${dictionary[7]}"
 
 
 
