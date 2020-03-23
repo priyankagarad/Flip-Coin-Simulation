d3639d398739bf9ac859d99c0c7f2a3dab54d269
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-23 17:33:13 +0530

    Refactor:calculated singlet combination, percentage and stored result in dictionary

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,23 +1,76 @@
+
 #!/bin/bash -x
-echo "Flip Coin Simulation"
-declare -A dictionary
-for (( i=0;i<10;i++ ))
-do
+echo "Welcome Flip Coin Simulation"
+
+#variable
+h=0
+t=0
+
+#declare dictionary
+declare -A dictionar
 	coin=$((RANDOM%2))
-	if [[ $coin -eq 1 ]]
+	if [[ $coin -eq 0 ]]
 	then
-		((head++))
+		printf "head"
 	else
-		((tail++))
+		printf "tail"
 	fi
+
+#combination of coin 
+functioncombination()
+{
+for (( i=0;i<$1;i++ ))
+do
+	string=" "
+	for(( j=0;j<$2;j++ ))
+	do
+		coin=$((RANDOM%2))
+		if [ $coin -eq 0 ]
+		then
+			string+=h
+		else
+			string+=t
+		fi
 done
 
-dictionary[0]=$head
-dictionary[1]=$tail
+dictionary[$string]=$(( ${dictionary[$string]}+1 ))
+done
 echo "${!dictionary[@]}":"${dictionary[@]}"
+}
 
-result1=$((($head*100)/10))
-result2=$((($tail*100)/10))
+#percentage  calculated
+
+function percentage()
+{
+	for keys in {!dictionary[@]}
+	{
+		percentage=`echo "scale=2; $((${dictionary[$keys]}))/$flipCoin*100" | bc`
+		echo "$keys = $percentage"
+   }
+}
+read -p  "How many time you flip coin" flipCoin
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
