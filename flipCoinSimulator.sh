3fce185e983f1f7cdf7ab2fb7f2f9515066ab113
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-23 18:04:49 +0530

    Refactor:created doublet combination and stored result in dictionary

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,45 +1,64 @@
 #!/bin/bash -x
 #VARIABLE
-HH=0
-HT=0
-TT=0
-TH=0
-flip=0
-
 echo "Flip Coin Simulation"
 declare -A dictionary
 
-#DOUBLET combination
-while [ $flip -lt 10 ]
-do
-	coin1=$((RANDOM%2))
-	coin2=$((RANDOM%2))
-	if [[ $coin1 -eq 1 && $coin2 -eq 1 ]]
-	then
-		((HH++))
+HEAD=0
+TAIL=1
 
-	elif [[ $coin1 -eq 1 && $coin2 -eq 0 ]]
-	then
-		((HT++))
+random=$((RANDOM%2))
+if [ $random -eq $HEAD ]
+then
+   printf "Head"
+else
+	printf "Tail"
+fi
 
-	elif [[ $coin1 -eq 0 && $coin -eq 1 ]]
-	then
-		((TH++))
+# Combinations of  coin
+function combination(){
+   for (( i=0;i<$1; i++ ))
+   do
+      string=""
+      for (( j=0;j<$2;j++ ))
+      do
+         random=$((RANDOM%2))
+         if [ $random -eq $HEAD ]
+         then
+            string+=H
+         else
+            string+=T
+         fi
+      done
+      dictionary[$string]=$(( ${dictionary[$string]}+1 ))
+   done
+   echo "All Combination : ${dictionary[@]}"
+   echo  "All keys : ${!dictionary[@]}"
+}
+#find percentage
+function percentageFind(){
+   for keys in ${!dictionary[@]}
+   {
+      persentage=`echo "scale=2; $((${dictionary[$keys]}))/$flipCoin*100" | bc`
+      echo  "$keys =  $persentage"
+   }
+}
+read -p  "How many time you flip coin" flipCoin
+read -p "Combination Single press 1) double press 2) Tripal press 3)" choice
 
-	else
-		((TT++))
-	fi
-	((flip++))
-done
-
-#percentage of doublet
-
-result1=`echo "scale=2; $HH*100/10" | bc `
-result2=`echo "scale=2; $HT*100/10" | bc `
-result3=`echo "scale=2; $TH*100/10" | bc `
-result4=`echo "scale=2; $TT*100/10" | bc `
-
-
-
-
+# call method
+if [ $choice -eq 1 ]
+then
+   combination $flipCoin $choice
+   percentageFind
+elif [ $choice -eq 2 ]
+then
+   combination $flipCoin $choice
+   percentageFind
+elif [ $choice  -eq 3 ]
+then
+   combination $flipCoin $choice
+   findPercentage
+else
+   printf "Please Enter a Right Choice" 
+fi
 
